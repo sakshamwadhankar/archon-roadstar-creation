f8560457eaab9d0c0665b6db08ebc88b5b0e34df
sakshamwadhankar <sakshamwadhankar21@gmail.com>
2025-08-23 17:13:50 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,14 @@ const Header = () => {
   return <header className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-md border-b border-border">
       <div className="container mx-auto px-6 py-4">
         <nav className="flex items-center justify-between">
-          <div className="flex items-center bg-slate-200 rounded-sm">
-            <img src="/lovable-uploads/961150df-a464-4cd4-8b84-82c7b536e57f.png" alt="Archon Roadsters Creation" className="h-12 w-auto object-contain" />
-          </div>
+      <div className="flex items-center">
+       <img
+         src="/lovable-uploads/961150df-a464-4cd4-8b84-82c7b536e57f.png?v=2"
+         alt="Archon Roadsters Creation"
+        className="h-12 w-auto object-contain"
+      />
+      </div>
+
           
           <div className="absolute left-1/2 transform -translate-x-1/2">
             <h1 className="font-bold text-amber-200 text-center mx-0 text-3xl">A R C H O N</h1>
